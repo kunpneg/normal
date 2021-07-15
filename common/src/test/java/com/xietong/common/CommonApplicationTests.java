465f81be9869e5ba0b1aac52964961f711d5500e
kunpeng <china_liuwenpeng@qq.com>
2021-07-15 21:29:15 +0800

    第一次提交

---
+++ common/src/test/java/com/xietong/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xietong.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
